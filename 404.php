d1351d22f790255ef7faa2dbe99996c1ec326488
Heinrich Strauss <git@hstrauss.co.za>
2014-04-23 19:51:46 +0200

    Commit at 2014.04.23-19h51

--- 404.php
+++ 404.php
@@ -14,9 +14,9 @@ get_header(); ?>
 <h3 class="arc-src"><span>Apologies, but the page you requested could not be found. Perhaps searching will help.</span></h3>
 
 <?php get_search_form(); ?><br />
-<p><a href="<?php echo home_url(); ?>" title="Browse the Home Page">&laquo; Or Return to the Home Page</a></p><br /><br />
+<p><a href="<?php echo home_url(); ?>" title="Browse the Home Page">&laquo; Or return to the Home Page</a></p><br /><br />
 
-<h2 class="post-title-color">You can also Visit the Following. These are the Featured Contents</h2>
+<h2 class="post-title-color">You can also visit the following Featured Posts:</h2>
 <div class="content-ver-sep"></div><br />
 <?php get_template_part( 'featured-box' ); ?>
  
--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 <title><?php wp_title() ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="icon" href="/favicon.png" sizes="16x16" type="image/png">
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
